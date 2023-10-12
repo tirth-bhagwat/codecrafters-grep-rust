8feb04b7d1e9f978642d8c1ddd969f05f94da54d
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-10-12 18:20:34 +0530

    Stage-8 End of string anchor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::io;
 use std::process;
-use std::str::Chars;
 
 enum Mode {
     SameCharacter(char),
@@ -13,77 +12,130 @@ enum Mode {
 }
 
 fn match_pattern(pattern: &str, input_line: &str) -> bool {
-    let mut inp = input_line.chars();
-    let mut patt = pattern.chars();
-    let mut mode = Mode::Unknown;
+    let inp: Vec<char> = input_line.chars().collect();
+    let mut patt: Vec<char> = pattern.chars().collect();
+    let inp_len = inp.len();
+    let patt_len = patt.len();
+
+    let mut chars_matched = 0_usize;
+    let mut reset_pattern = false;
 
     let mut match_only_next = false;
 
-    while let Some(x) = patt.next() {
+    let mut i = 0;
+    let mut j = 0;
+    while i < patt_len || reset_pattern {
+        if reset_pattern {
+            i = 0;
+            reset_pattern = false;
+            chars_matched = 0;
+            match_only_next = false;
+        }
+
+        let x = patt[i];
+        i += 1;
         match x {
             '^' => {
                 match_only_next = true;
                 continue;
             }
+            '$' => {
+                return if chars_matched > 0 && j >= inp_len {
+                    true
+                } else {
+                    false
+                }
+            }
             _ => {}
         }
-
-        if let Some(m) = set_mode(&mut patt, x) {
-            mode = m;
-        } else {
-            return false;
-        }
-
-        loop {
-            if let Some(y) = inp.next() {
-                match &mode {
-                    Mode::SameCharacter(ch) => {
-                        if &y == ch {
-                            match_only_next = true;
-                            break;
-                        } else if match_only_next {
-                            return false;
+        match get_mode(&mut patt, x, &mut i) {
+            Some(m) => loop {
+                if j < inp_len {
+                    let y = inp[j];
+                    j += 1;
+                    match &m {
+                        Mode::SameCharacter(ch) => {
+                            if &y == ch {
+                                match_only_next = true;
+                                chars_matched += 1;
+                                break;
+                            } else if match_only_next {
+                                if chars_matched > 0 {
+                                    reset_pattern = true;
+                                    j -= 1;
+                                    break;
+                                }
+                                return false;
+                            }
                         }
-                    }
-                    Mode::Alphanumeric => {
-                        if y.is_ascii_alphanumeric() || y == '_' {
-                            match_only_next = true;
-                            break;
-                        } else if match_only_next {
-                            return false;
+                        Mode::Alphanumeric => {
+                            if y.is_ascii_alphanumeric() || y == '_' {
+                                match_only_next = true;
+                                chars_matched += 1;
+                                break;
+                            } else if match_only_next {
+                                if chars_matched > 0 {
+                                    reset_pattern = true;
+                                    j -= 1;
+                                    break;
+                                }
+                                return false;
+                            }
                         }
-                    }
-                    Mode::PosCharGroup(accepted) => {
-                        if accepted.contains(&y) {
-                            match_only_next = true;
-                            break;
-                        } else if match_only_next {
-                            return false;
+                        Mode::PosCharGroup(accepted) => {
+                            if accepted.contains(&y) {
+                                match_only_next = true;
+                                chars_matched += 1;
+                                break;
+                            } else if match_only_next {
+                                if chars_matched > 0 {
+                                    reset_pattern = true;
+                                    j -= 1;
+                                    break;
+                                }
+                                return false;
+                            }
                         }
-                    }
-                    Mode::NegCharGroup(not_accepted) => {
-                        if !not_accepted.contains(&y) {
-                            match_only_next = true;
-                            break;
-                        } else if match_only_next {
-                            return false;
+                        Mode::NegCharGroup(not_accepted) => {
+                            if !not_accepted.contains(&y) {
+                                match_only_next = true;
+                                chars_matched += 1;
+                                break;
+                            } else if match_only_next {
+                                if chars_matched > 0 {
+                                    reset_pattern = true;
+                                    j -= 1;
+                                    break;
+                                }
+                                return false;
+                            }
                         }
-                    }
-                    Mode::Number => {
-                        if y.is_ascii_digit() {
-                            match_only_next = true;
-                            break;
-                        } else if match_only_next {
-                            return false;
+                        Mode::Number => {
+                            if y.is_ascii_digit() {
+                                match_only_next = true;
+                                chars_matched += 1;
+                                break;
+                            } else if match_only_next {
+                                if chars_matched > 0 {
+                                    reset_pattern = true;
+                                    j -= 1;
+                                    break;
+                                }
+                                return false;
+                            }
                         }
+                        Mode::Unknown => return false,
                     }
-                    Mode::Unknown => return false,
+                } else {
+                    return false;
                 }
-            } else {
+            },
+            None => {
                 return false;
             }
         }
     }
+
     true
 }
 
@@ -111,10 +163,13 @@ fn main() {
     }
 }
 
-fn set_mode(mut patt: &mut Chars, x: char) -> Option<Mode> {
+fn get_mode(patt: &mut Vec<char>, x: char, i: &mut usize) -> Option<Mode> {
+    let patt_len = patt.len();
     return match x {
         '\\' => {
-            if let Some(u) = patt.next() {
+            if *i < patt_len {
+                let u = patt[*i];
+                *i += 1;
                 return match u {
                     'w' => Some(Mode::Alphanumeric),
                     'd' => Some(Mode::Number),
@@ -127,7 +182,9 @@ fn set_mode(mut patt: &mut Chars, x: char) -> Option<Mode> {
             let mut accepted: Vec<char> = vec![];
             let mut pos = true;
 
-            if let Some(ch) = patt.next() {
+            if *i < patt_len {
+                let ch = patt[*i];
+                *i += 1;
                 if ch == '^' {
                     pos = false;
                 } else {
@@ -136,18 +193,21 @@ fn set_mode(mut patt: &mut Chars, x: char) -> Option<Mode> {
             }
 
             accepted.append(
-                (&mut patt)
-                    .take_while(|ch| *ch != ']')
+                patt.clone()[*i..]
+                    .iter()
+                    .take_while(|ch| **ch != ']')
+                    .map(|x| *x)
                     .collect::<Vec<char>>()
                     .as_mut(),
             );
             if pos {
+                *i += accepted.len();
                 Some(Mode::PosCharGroup(accepted))
             } else {
+                *i += accepted.len() + 1;
                 Some(Mode::NegCharGroup(accepted))
             }
         }
-
         _ => Some(Mode::SameCharacter(x)),
     };
 }
@@ -161,6 +221,7 @@ mod tests {
         assert_eq!(match_pattern("abc", "abc"), true, "Test 1");
         assert_eq!(match_pattern("abc", "aibic"), false, "Test 2");
         assert_eq!(match_pattern("abc", "aaibicj"), false, "Test 3");
+        assert_eq!(match_pattern("abc", "aabcibicj"), true, "Test 3b");
         assert_eq!(match_pattern("abc", "abcdef"), true, "Test 4");
         assert_eq!(match_pattern("abc", "defabc"), true, "Test 5");
         assert_eq!(match_pattern("abc", "defabcdef"), true, "Test 6");
@@ -198,6 +259,11 @@ mod tests {
         assert_eq!(match_pattern("[xyz]", "XYZ"), false, "Test 3");
         assert_eq!(match_pattern("[aeiou]", "consonants"), true, "Test 4");
         assert_eq!(match_pattern("[aeiou]", "rhythm"), false, "Test 5");
+        assert_eq!(
+            match_pattern("[aeiou][aeiou]", "consonants"),
+            false,
+            "Test 6"
+        );
         // assert_eq!(match_pattern("[a-z]", "abcdefg"), true, "Extra_1");
         // assert_eq!(match_pattern("[0-9]", "alpha123"), true, "Extra_2");
     }
@@ -209,7 +275,7 @@ mod tests {
         assert_eq!(match_pattern("[^pqr]", "apple"), true, "Test 3");
         assert_eq!(
             match_pattern("[^aeiou][^aeiou]", "consonants"),
-            false,
+            true,
             "Test 4"
         );
         assert_eq!(match_pattern("[^aeiou]", "rhythm"), true, "Test 5");
@@ -248,5 +314,17 @@ mod tests {
         assert_eq!(match_pattern("^123", "456123"), false, "Test 6");
         // assert_eq!(match_pattern("^ab", "abcd\nefgh"), true, "Test 7");
         // assert_eq!(match_pattern("^cd", "abcd\nefgh"), false, "Test 8");
+    }
+
+    #[test]
+    fn test_stage_8() {
+        assert_eq!(match_pattern("dog$", "dog"), true, "Test 1");
+        assert_eq!(match_pattern("dog$", "dogs"), false, "Test 2");
+        assert_eq!(match_pattern("pie$", "apple pie"), true, "Test 3");
+        assert_eq!(match_pattern("apple$", "pie apple"), true, "Test 4");
+        assert_eq!(match_pattern("123$", "123456"), false, "Test 5");
+        assert_eq!(match_pattern("123$", "456123"), true, "Test 6");
+        assert_eq!(match_pattern("efgh$", "abcd\nefgh"), true, "Test 7");
+        assert_eq!(match_pattern("abcd$", "abcd\nefgh"), false, "Test 8");
     }
 }
